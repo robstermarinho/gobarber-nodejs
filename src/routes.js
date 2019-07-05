054f17d09fbb67f44edfad9e9c4f989aa64e811c
Robert Marinho <robstermarinho@gmail.com>
2019-07-04 22:06:44 -0300

    Create User Model

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/Users';
 
 const routes = new Router();
 
-routes.get('/', (req, res) =>
-  res.json({
-    msg: 'Hello!',
-  })
-);
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Robert Marinho',
+    email: 'robster.mr@gmail.com',
+    password_hash: '123456',
+  });
+
+  return res.json(user);
+});
 
 export default routes;
