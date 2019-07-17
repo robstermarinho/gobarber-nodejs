00d1b84f6323e325cd54e2545297502852868bae
Robert Marinho <robstermarinho@gmail.com>
2019-07-17 00:11:15 -0300

    List Availabel Times to Schedule

--- src/routes.js
+++ src/routes.js
@@ -10,6 +10,7 @@ import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
 import ScheduleController from './app/controllers/ScheduleController';
 import NotificationsController from './app/controllers/NotificationsController';
+import AvailableController from './app/controllers/AvailableController';
 
 const routes = new Router();
 const upload = new Multer(multerConfig);
@@ -28,14 +29,18 @@ routes.post('/sessions', SessionController.store);
 // It will use the auth middleware for the other routes bellow:
 routes.use(authMiddleware);
 routes.put('/users', UserController.update);
+
 routes.get('/providers', ProviderController.index);
+routes.get('/providers/:providerId/available', AvailableController.index);
 
 routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
 routes.delete('/appointments/:id', AppointmentController.delete);
 
 routes.get('/schedule', ScheduleController.index);
+
 routes.get('/notifications', NotificationsController.index);
 routes.put('/notifications/:id', NotificationsController.update);
+
 routes.post('/files', upload.single('file'), FileController.store);
 export default routes;
